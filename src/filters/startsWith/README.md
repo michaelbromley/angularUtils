5b7e856fe56d04f4589369579ebe4b6d0e617a25
Michael Bromley <michael@michaelbromley.co.uk>
2014-03-27 21:56:34 +0100

    add startsWith filter

---
+++ src/filters/startsWith/README.md
@@ -0,0 +1,14 @@
+# startsWith Filter
+
+Dead simple - whereas the standard AngularJS `filter` filter will return a match if the search string appears anywhere within the test string, this filter only returns a match if the test string *starts with* the search string.
+
+For example:
+
+    // in your controller
+    $scope.things = ['cathode', 'house', 'chomp'];
+    $scope.search = "ho";
+
+    // in your template
+    <li ng-repeat="thing in things | filter: search">{{thing}}</li> // cathode, house, chomp
+
+    <li ng-repeat="thing in things | startsWith: search">{{thing}}</li> // house
--- src/filters/startsWith/startsWith.spec.js
+++ src/filters/startsWith/startsWith.spec.js
@@ -1,3 +1,29 @@
 /**
  * Created by Michael on 27/03/14.
  */
+describe('startsWith filter', function() {
+    var startsWithFilter;
+    var testArray;
+
+    beforeEach(module('angularUtils'));
+    beforeEach(inject(function(_$filter_) {
+        testArray = [
+            'cake',
+            'hammer',
+            'cup',
+            'earth',
+            'apple',
+            'tap'
+        ];
+        startsWithFilter = _$filter_('startsWith');
+    }));
+
+    it('should should return just the items starting with the search string', function() {
+        expect(startsWithFilter(testArray, 'c')).toEqual(['cake', 'cup']);
+    });
+
+    it('should not return items if the search appears mid way through the string', function() {
+        expect(startsWithFilter(testArray, 'a')).toEqual(['apple']);
+    });
+
+});
