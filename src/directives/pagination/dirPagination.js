795d91d05ba5a43383a7dd4821c308b4f1041a53
mpkorstanje <rien.korstanje@gmail.com>
2015-03-24 22:03:00 +0100

    Pagination template is statically loaded
    Pagination template is statically loaded from the cache now.
    
    The pagination template is inserted into the `$templateCache` at the url
    `directives/pagination/dirPagination.tpl.html` for backwards
    compatability.
    
    'dirPagination.tpl.html' can *not* be deleted because any references to
    that file may be prefixed with a package folder. e.g.
    `bower_components/directives/pagination/dirPagination.tpl.html`

--- src/directives/pagination/dirPagination.js
+++ src/directives/pagination/dirPagination.js
@@ -39,7 +39,8 @@
         .directive('dirPaginationControls', ['paginationService', 'paginationTemplate', dirPaginationControlsDirective])
         .filter('itemsPerPage', ['paginationService', itemsPerPageFilter])
         .service('paginationService', paginationService)
-        .provider('paginationTemplate', paginationTemplateProvider);
+        .provider('paginationTemplate', paginationTemplateProvider)
+        .run(['$templateCache',dirPaginationControlsTemplateInstaller]);
 
     function dirPaginateDirective($compile, $parse, paginationService) {
 
@@ -206,6 +207,10 @@
             priority: 5000,
             terminal: true
         };
+    }
+
+    function dirPaginationControlsTemplateInstaller($templateCache) {
+        $templateCache.put('directives/pagination/dirPagination.tpl.html', '<ul class="pagination" ng-if="1 < pages.length"><li ng-if="boundaryLinks" ng-class="{ disabled : pagination.current == 1 }"><a href="" ng-click="setCurrent(1)">&laquo;</a></li><li ng-if="directionLinks" ng-class="{ disabled : pagination.current == 1 }"><a href="" ng-click="setCurrent(pagination.current - 1)">&lsaquo;</a></li><li ng-repeat="pageNumber in pages track by $index" ng-class="{ active : pagination.current == pageNumber, disabled : pageNumber == '...' }"><a href="" ng-click="setCurrent(pageNumber)">{{ pageNumber }}</a></li><li ng-if="directionLinks" ng-class="{ disabled : pagination.current == pagination.last }"><a href="" ng-click="setCurrent(pagination.current + 1)">&rsaquo;</a></li><li ng-if="boundaryLinks"  ng-class="{ disabled : pagination.current == pagination.last }"><a href="" ng-click="setCurrent(pagination.last)">&raquo;</a></li></ul>');
     }
 
     function dirPaginationControlsDirective(paginationService, paginationTemplate) {
