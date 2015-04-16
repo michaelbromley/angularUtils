a6dd16c3c49f6f12ecd16f582c99b37964f3923e
root <root@localhost.localdomain>
2015-04-16 17:46:27 -0600

    Disqus SSO

--- src/directives/disqus/dirDisqus.js
+++ src/directives/disqus/dirDisqus.js
@@ -35,6 +35,8 @@
                 disqus_category_id: '@disqusCategoryId',
                 disqus_disable_mobile: '@disqusDisableMobile',
                 disqus_config_language : '@disqusConfigLanguage',
+                disqus_remote_auth_s3 : '@disqusRemoteAuthS3',
+                disqus_api_key : '@disqusApiKey',
                 readyToBind: "@"
             },
             template: '<div id="disqus_thread"></div><a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>',
@@ -54,6 +56,7 @@
                         isReady = "true";
                     }
                     if (scope.$eval(isReady)) {
+                      console.log('remote'+scope.disqus_remote_auth_s3);
                         // put the config variables into separate global vars so that the Disqus script can see them
                         $window.disqus_shortname = scope.disqus_shortname;
                         $window.disqus_identifier = scope.disqus_identifier;
@@ -63,6 +66,9 @@
                         $window.disqus_disable_mobile = scope.disqus_disable_mobile;
                         $window.disqus_config =  function () {
                             this.language = scope.disqus_config_language;
+                            this.page.remote_auth_s3 = scope.disqus_remote_auth_s3;
+                            this.page.api_key =scope.disqus_api_key;
+
                         };
                         // get the remote Disqus script and insert it into the DOM, but only if it not already loaded (as that will cause warnings)
                         if (!$window.DISQUS) {
@@ -77,6 +83,8 @@
                                     this.page.url = scope.disqus_url;
                                     this.page.title = scope.disqus_title;
                                     this.language = scope.disqus_config_language;
+                                    this.page.remote_auth_s3=scope.disqus_remote_auth_s3;
+                                    this.page.api_key=scope.disqus_api_key;
                                 }
                             });
                         }
