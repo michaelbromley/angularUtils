b2a5dd4d31fdeb9076a045e0de49c37af8d74492
Michael Bromley <michael@michaelbromley.co.uk>
2016-01-22 10:05:05 +0100

    Fix https://github.com/michaelbromley/angularUtils/issues/286

--- src/directives/pagination/dirPagination.js
+++ src/directives/pagination/dirPagination.js
@@ -238,7 +238,9 @@
 
             if (!paginationService.isRegistered(paginationId) && !paginationService.isRegistered(rawId)) {
                 var idMessage = (paginationId !== DEFAULT_ID) ? ' (id: ' + paginationId + ') ' : ' ';
-                console.warn('Pagination directive: the pagination controls' + idMessage + 'cannot be used without the corresponding pagination directive, which was not found at link time.');
+                if (window.console) {
+                    console.warn('Pagination directive: the pagination controls' + idMessage + 'cannot be used without the corresponding pagination directive, which was not found at link time.');
+                }
             }
 
             if (!scope.maxSize) { scope.maxSize = 9; }
