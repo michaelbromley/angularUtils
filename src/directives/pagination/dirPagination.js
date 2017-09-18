8ba724c7e604acdae25ce11300e2dde0371c6610
Adil Khan <adil.nisar@tkxel.com>
2017-09-18 12:26:28 +0500

    changing the behavior of the pagination dots to open to expand the next three pages instead of being disbaled.

--- src/directives/pagination/dirPagination.js
+++ src/directives/pagination/dirPagination.js
@@ -33,6 +33,7 @@
         .service('paginationService', paginationService)
         .provider('paginationTemplate', paginationTemplateProvider)
         .run(['$templateCache',dirPaginationControlsTemplateInstaller]);
+        var goToPageFor = null;
 
     function dirPaginateDirective($compile, $parse, paginationService) {
 
@@ -222,7 +223,7 @@
     }
 
     function dirPaginationControlsTemplateInstaller($templateCache) {
-        $templateCache.put('angularUtils.directives.dirPagination.template', '<ul class="pagination" ng-if="1 < pages.length || !autoHide"><li ng-if="boundaryLinks" ng-class="{ disabled : pagination.current == 1 }"><a href="" ng-click="setCurrent(1)">&laquo;</a></li><li ng-if="directionLinks" ng-class="{ disabled : pagination.current == 1 }"><a href="" ng-click="setCurrent(pagination.current - 1)">&lsaquo;</a></li><li ng-repeat="pageNumber in pages track by tracker(pageNumber, $index)" ng-class="{ active : pagination.current == pageNumber, disabled : pageNumber == \'...\' || ( ! autoHide && pages.length === 1 ) }"><a href="" ng-click="setCurrent(pageNumber)">{{ pageNumber }}</a></li><li ng-if="directionLinks" ng-class="{ disabled : pagination.current == pagination.last }"><a href="" ng-click="setCurrent(pagination.current + 1)">&rsaquo;</a></li><li ng-if="boundaryLinks"  ng-class="{ disabled : pagination.current == pagination.last }"><a href="" ng-click="setCurrent(pagination.last)">&raquo;</a></li></ul>');
+        $templateCache.put('angularUtils.directives.dirPagination.template', '<ul class="pagination" ng-if="1 < pages.length || !autoHide"><li ng-if="boundaryLinks" ng-class="{ disabled : pagination.current == 1 }"><a href="" ng-click="setCurrent(1)">&laquo;</a></li><li ng-if="directionLinks" ng-class="{ disabled : pagination.current == 1 }"><a href="" ng-click="setCurrent(pagination.current - 1)">&lsaquo;</a></li><li ng-repeat="pageNumber in pages track by tracker(pageNumber, $index)" ng-class="{ active : pagination.current == pageNumber || ( ! autoHide && pages.length === 1 ) }"><a href="" ng-click="setCurrent(pageNumber)">{{ pageNumber }}</a></li><li ng-if="directionLinks" ng-class="{ disabled : pagination.current == pagination.last }"><a href="" ng-click="setCurrent(pagination.current + 1)">&rsaquo;</a></li><li ng-if="boundaryLinks"  ng-class="{ disabled : pagination.current == pagination.last }"><a href="" ng-click="setCurrent(pagination.last)">&raquo;</a></li></ul>');
     }
 
     function dirPaginationControlsDirective(paginationService, paginationTemplate) {
@@ -331,6 +332,9 @@
                     num = parseInt(num, 10);
                     paginationService.setCurrentPage(paginationId, num);
                 }
+                else {
+                    paginationService.setCurrentPage(paginationId, goToPageFor);
+                }
             };
 
             /**
@@ -362,6 +366,7 @@
                         });
                     }
                 }
+                
             }
 
             function generatePagination() {
@@ -431,6 +436,7 @@
                 var closingEllipsesNeeded = (i === paginationRange - 1 && (position === 'middle' || position === 'start'));
                 if (ellipsesNeeded && (openingEllipsesNeeded || closingEllipsesNeeded)) {
                     pages.push('...');
+                    goToPageFor = pageNumber;
                 } else {
                     pages.push(pageNumber);
                 }
@@ -637,3 +643,4 @@
         };
     }
 })();
+
